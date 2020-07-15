69686934258c34fd9120a9b83e4512a37f9e2d2c
czuli <lukasz.czulak@gmail.com>
2020-07-15 12:40:23 +0200

    ssh add buddy.yml

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h3>Learn React</h3>
+        <h3>Learn React123</h3>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
